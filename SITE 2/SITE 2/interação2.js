dd8a849b4b2c30dd339b31fa1e1ebeea2df60164
caiosldn <153527302+caiosldn@users.noreply.github.com>
2024-04-05 08:46:47 -0300

    Add files via upload

---
+++ SITE 2/SITE 2/interação2.js
@@ -0,0 +1,9 @@
+function wikipedia() {
+	 window.location.replace("https://pt.wikipedia.org/wiki/Pinguim");
+}
+function uol() {
+	 window.location.replace("https://brasilescola.uol.com.br/animais/pinguins.htm");
+}
+function rol() {
+	 window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
